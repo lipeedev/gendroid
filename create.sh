607d126bcca257b240411cb828193c625767cb09
lipeedev <anamariafakezaum@gmail.com>
2023-12-17 01:34:24 +0000

    feat: add kotlin structure

--- create.sh
+++ create.sh
@@ -20,33 +20,42 @@ if ! [ -v JAVA_HOME ]; then
    exit 1
 fi
 
-files_dir="$(dirname $0)/files"
-
 read -p "- App name: " app_name 
 read -p "- Group: " group_name
 package="$group_name.$app_name"
 
-mkdir -p $app_name 
+mkdir -p $app_name
 
-cp -r $files_dir/* $app_name
+files_dir="$(dirname $0)/files/java"
+read -p "This project uses Kotlin? [y/N] " is_kotlin
 
-sed -i "s#(APP_NAME)#$app_name#g"  $app_name/settings.gradle
+  case "$is_kotlin" in
+  [yY][eE][sS]|[yY])
+ 
+  files_dir="$(dirname $0)/files/kotlin"
+  cp -r $files_dir/* $app_name
+  sed -i "s#(APP_PACKAGE)#$package#g" $app_name/app/src/main/kotlin/MainActivity.kt
+  package_dir=$app_name/app/src/main/kotlin/$(echo $package | sed "s/\./\//g")
+  default_package_dir=$app_name/app/src/main/kotlin
+   
+  ;;
+    *)
+    
+  cp -r $files_dir/* $app_name
+  sed -i "s#(APP_PACKAGE)#$package#g" $app_name/app/src/main/java/MainActivity.kt
+  package_dir=$app_name/app/src/main/java/$(echo $package | sed "s/\./\//g")
+  default_package_dir=$app_name/app/src/main/java
+   
+  ;;
+  esac 
 
-sed -i "s#(ANDROID_SDK)#$ANDROID_HOME#g" $app_name/gradle.properties
-sed -i "s#(JAVA_HOME)#$JAVA_HOME#g" $app_name/gradle.properties
+  mkdir -p $package_dir
 
-sed -i "s#(APP_ID)#$package#g" $app_name/app/build.gradle
-
-sed -i "s#(APP_NAME)#$app_name#g" $app_name/app/src/main/AndroidManifest.xml
-
-sed -i "s#(APP_PACKAGE)#$package#g" $app_name/app/src/main/java/MainActivity.java
-
-package_dir=$app_name/app/src/main/java/$(echo $package | sed "s/\./\//g")
-
-mkdir -p $package_dir
-
-default_package_dir=$app_name/app/src/main/java
-
-mv $default_package_dir/MainActivity.java $package_dir 
-
+  sed -i "s#(APP_NAME)#$app_name#g"  $app_name/settings.gradle
+  sed -i "s#(ANDROID_SDK)#$ANDROID_HOME#g" $app_name/gradle.properties
+  sed -i "s#(JAVA_HOME)#$JAVA_HOME#g" $app_name/gradle.properties
+  sed -i "s#(APP_NAME)#$app_name#g" $app_name/app/src/main/AndroidManifest.xml
+  sed -i "s#(APP_ID)#$package#g" $app_name/app/build.gradle
+ 
+  mv $default_package_dir/MainActivity.* $package_dir 
 }
